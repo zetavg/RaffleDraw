dca04270ffa1ed0ee481bd2ef35197f127ed3598
Neson <neson@dex.tw>
2014-05-16 11:30:43 +0800

    init

---
+++ data.demo.js
@@ -0,0 +1,511 @@
+/**
+ * Demo data for RaffleDraw
+ */
+
+var demoData = [
+    {
+        "id": "000-00-001",
+        "name": "陳俞安",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-002",
+        "name": "陳凡睿",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-003",
+        "name": "宋樂玲",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-004",
+        "name": "王嘉秋",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-005",
+        "name": "鍾明杰",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-006",
+        "name": "嵇庭哲",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-007",
+        "name": "鄭怡君",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-008",
+        "name": "李柏季",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-009",
+        "name": "周佳霖",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-010",
+        "name": "陸筱榮",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-011",
+        "name": "童家榮",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-012",
+        "name": "黃怡君",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-013",
+        "name": "井易仰",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-014",
+        "name": "陳火香",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-015",
+        "name": "劉倍蕙",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-016",
+        "name": "毛信豪",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-017",
+        "name": "黃佩玲",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-018",
+        "name": "張宗穎",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-019",
+        "name": "吳怡靜",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-020",
+        "name": "潘依婷",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-021",
+        "name": "曹枝孜",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-022",
+        "name": "荊永谷",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-023",
+        "name": "陸揚德",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-024",
+        "name": "陳宛蓉",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-025",
+        "name": "姜俊吉",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-026",
+        "name": "林天定",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-027",
+        "name": "鄭家瑋",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-028",
+        "name": "林寶杰",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-029",
+        "name": "王姿江",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-030",
+        "name": "楊岳貞",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-031",
+        "name": "吳意婷",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-032",
+        "name": "梁雅玲",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-033",
+        "name": "陳信韋",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-034",
+        "name": "陳中銘",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-035",
+        "name": "高倩瑞",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-036",
+        "name": "席文馨",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-037",
+        "name": "鄭俐亞",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-038",
+        "name": "張新全",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-039",
+        "name": "蔡志柏",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-040",
+        "name": "李初昇",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-041",
+        "name": "黃智玉",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-042",
+        "name": "邱沛齊",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-043",
+        "name": "汪幼宣",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-044",
+        "name": "童倫伶",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-045",
+        "name": "林美慧",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-046",
+        "name": "楊淑娟",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-047",
+        "name": "林家珮",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-048",
+        "name": "陽意霞",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-049",
+        "name": "林耀原",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-050",
+        "name": "陳雅雯",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-051",
+        "name": "許亞淑",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-052",
+        "name": "謝信宏",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-053",
+        "name": "徐詩寶",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-054",
+        "name": "趙郁婷",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-055",
+        "name": "林雅坤",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-056",
+        "name": "陳家榮",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-057",
+        "name": "林怡君",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-058",
+        "name": "毛思翰",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-059",
+        "name": "黃純軍",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-060",
+        "name": "陳俞然",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-061",
+        "name": "周志生",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-062",
+        "name": "姜沛慧",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-063",
+        "name": "李怡迪",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-064",
+        "name": "林智仁",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-065",
+        "name": "黃恩春",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-066",
+        "name": "蔡豐軍",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-067",
+        "name": "黃燕宏",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-068",
+        "name": "潘珮君",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-069",
+        "name": "林君冰",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-070",
+        "name": "蔡建宏",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-071",
+        "name": "丁偉哲",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-072",
+        "name": "王白盛",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-073",
+        "name": "詹冠儒",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-074",
+        "name": "黃景恆",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-075",
+        "name": "楊玉木",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-076",
+        "name": "許俊宏",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-077",
+        "name": "蔡雅茹",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-078",
+        "name": "陳祥芃",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-079",
+        "name": "游庭彬",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-080",
+        "name": "林昆修",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-081",
+        "name": "吳逸群",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-082",
+        "name": "林敬屏",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-083",
+        "name": "林逸芝",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-084",
+        "name": "袁昌富",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-085",
+        "name": "閻登純",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-086",
+        "name": "張心怡",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-087",
+        "name": "方冠妮",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-088",
+        "name": "陳亭云",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-089",
+        "name": "張琦行",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-090",
+        "name": "陳宗穎",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-091",
+        "name": "郭俊仁",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-092",
+        "name": "汪祖仰",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-093",
+        "name": "丁建廷",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-094",
+        "name": "林展彥",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-095",
+        "name": "黃美雲",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-096",
+        "name": "葉士瑜",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-097",
+        "name": "張松昕",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-098",
+        "name": "鄭浩聖",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-099",
+        "name": "張柏青",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-100",
+        "name": "蔡皓恒",
+        "avator": "http://placehold.it/150x150"
+    },
+    {
+        "id": "000-00-101",
+        "name": "王俊豪",
+        "avator": "http://placehold.it/150x150"
+    }
+]
